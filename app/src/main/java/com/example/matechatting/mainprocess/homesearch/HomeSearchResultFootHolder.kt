05cef66b760f8d403f44f141c4fc154375589af2
TKW <2233014844@qq.com>
2019-08-30 02:23:36 +0800

    除方向选择、侧边搜索、通讯录搜索、头像加载外，基本完成

--- app/src/main/java/com/example/matechatting/mainprocess/homesearch/HomeSearchResultFootHolder.kt
+++ app/src/main/java/com/example/matechatting/mainprocess/homesearch/HomeSearchResultFootHolder.kt
@@ -7,5 +7,7 @@ import com.example.matechatting.databinding.ItemSearchResultFootBinding
 class HomeSearchResultFootHolder(private val binding: ItemSearchResultFootBinding) : BaseHolder(binding) {
     fun getView(): FrameLayout {
         return binding.resultFootLayout
+
+
     }
 }
